0af991094ebfef35bfd3bc11c97545bf6b26bf46
Eddie Naff <eddienaff@gmail.com>
2023-03-30 16:51:18 -0700

    Add basic project setup code

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,48 @@
 import "./style.css";
-import { setupCounter } from "./counter";
+import * as THREE from "three";
+import { OrbitControls } from "three-full/sources/controls/OrbitControls.js";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-  
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+interface PerspectiveCameraArgs {
+    fov?: number | undefined;
+    aspect?: number | undefined;
+    near?: number | undefined;
+    far?: number | undefined;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const config: PerspectiveCameraArgs = {
+    fov: 75,
+    aspect: window.innerWidth / window.innerHeight,
+    near: 0.1,
+    far: 1000,
+};
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    config.fov,
+    config.aspect,
+    config.near,
+    config.far
+);
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
+
+animate();
