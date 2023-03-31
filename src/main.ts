85232d4a51405b64ea8b617f697f223a2243dd67
Eddie Naff <eddienaff@gmail.com>
2023-03-30 17:51:04 -0700

    Switch mesh material

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,8 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
