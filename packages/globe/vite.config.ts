097ad6f69beb946a3d8b0c9ad8957bf141649a4f
Eddie Naff <eddienaff@gmail.com>
2023-04-01 14:21:10 -0700

    Re-configure vite for glsl

--- packages/globe/vite.config.ts
+++ packages/globe/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import vitePluginGlsl from "vite-plugin-glsl";
 
 const config = defineConfig({
-  root: "./src",
+    root: "./src",
+    plugins: [vitePluginGlsl()],
 });
 
 export default config;
