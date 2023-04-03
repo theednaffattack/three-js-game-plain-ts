4b2e786dab941ca4d70d483e1870b0f0f551d06d
Eddie Naff <eddienaff@gmail.com>
2023-04-03 10:47:03 -0700

    Add camera, scene, and renderer

--- packages/basics/src/main.ts
+++ packages/basics/src/main.ts
@@ -1,1 +1,21 @@
+import * as THREE from "three";
+import { PerspectiveCameraArgs } from "shared/types";
+
 import "./style.css";
+
+const configPerspectiveCamera: PerspectiveCameraArgs = {
+    fov: 75,
+    aspect: innerWidth / innerHeight,
+    near: 0.1,
+    far: 1000,
+};
+
+const config = { cam: configPerspectiveCamera };
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    config.cam.fov,
+    config.cam.aspect,
+    config.cam.near,
+    config.cam.far
+);
+const renderer = new THREE.WebGL1Renderer();
