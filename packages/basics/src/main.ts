cd4d71fe1506b50588acb146daed462ca60b9d8c
Eddie Naff <eddienaff@gmail.com>
2023-04-03 15:19:12 -0700

    Replace box geometry with plane geometry

--- packages/basics/src/main.ts
+++ packages/basics/src/main.ts
@@ -1,22 +1,24 @@
 import * as THREE from "three";
-import { PerspectiveCameraArgs } from "shared/types";
+import { PerspectiveCameraParams, PlaneGeometryParams } from "shared/types";
 
 import "./style.css";
+import { animate } from "./animate";
 
-const configPerspectiveCamera: PerspectiveCameraArgs = {
+const configPerspectiveCamera: PerspectiveCameraParams = {
     fov: 75,
     aspect: innerWidth / innerHeight,
     near: 0.1,
     far: 1000,
 };
 
-const configBoxGeometry = {
-    width: 1,
-    length: 1,
-    height: 1,
+const configPlaneGeometry: PlaneGeometryParams = {
+    width: 5,
+    height: 5,
+    widthSegments: 10,
+    heightSegments: 10,
 };
 
-const config = { cam: configPerspectiveCamera, box: configBoxGeometry };
+const config = { cam: configPerspectiveCamera, plane: configPlaneGeometry };
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -33,19 +35,45 @@ if (!app) {
     throw new Error(`Unable to sense a DOM Element named '${appReference}'!`);
 }
 
-const renderer = new THREE.WebGL1Renderer();
+export const renderer = new THREE.WebGL1Renderer({
+    antialias: true,
+});
 
 renderer.setSize(app.offsetWidth, app.offsetHeight);
+renderer.setPixelRatio(devicePixelRatio);
 
 app.appendChild(renderer.domElement);
 
-const boxGeometry = new THREE.BoxGeometry(
-    config.box.width,
-    config.box.height,
-    config.box.length
+const planeGeometry = new THREE.PlaneGeometry(
+    config.plane.width,
+    config.plane.height,
+    config.plane.widthSegments,
+    config.plane.heightSegments
 );
+const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+    flatShading: true,
+});
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
 
-const mesh = new THREE.Mesh(boxGeometry, material);
-scene.add(mesh);
+// @ts-ignore
+const planeMeshArr = planeMesh.geometry.attributes.position.array;
+
+for (let index = 0; index < planeMeshArr.length; index += 3) {
+    const x = planeMeshArr[index];
+    const y = planeMeshArr[index + 1];
+    const z = planeMeshArr[index + 2];
+
+    planeMeshArr[index + 2] = z + Math.random();
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+camera.position.z = 5;
+
+animate({ scene, camera });
