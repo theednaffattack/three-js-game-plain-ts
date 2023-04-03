c0038825f0674af79fa639e7bdb2a228a3bd86da
Eddie Naff <eddienaff@gmail.com>
2023-04-03 11:06:52 -0700

    Add geometry, mesh, and material to scene

--- packages/basics/src/main.ts
+++ packages/basics/src/main.ts
@@ -10,7 +10,14 @@ const configPerspectiveCamera: PerspectiveCameraArgs = {
     far: 1000,
 };
 
-const config = { cam: configPerspectiveCamera };
+const configBoxGeometry = {
+    width: 1,
+    length: 1,
+    height: 1,
+};
+
+const config = { cam: configPerspectiveCamera, box: configBoxGeometry };
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     config.cam.fov,
@@ -18,4 +25,27 @@ const camera = new THREE.PerspectiveCamera(
     config.cam.near,
     config.cam.far
 );
+
+const appReference = "#app";
+const app = document.querySelector<HTMLDivElement>(appReference);
+
+if (!app) {
+    throw new Error(`Unable to sense a DOM Element named '${appReference}'!`);
+}
+
 const renderer = new THREE.WebGL1Renderer();
+
+renderer.setSize(app.offsetWidth, app.offsetHeight);
+
+app.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(
+    config.box.width,
+    config.box.height,
+    config.box.length
+);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+scene.add(mesh);
