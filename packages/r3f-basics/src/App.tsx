d35bd9c6ce6d87dd84b1a68843e34e7cb2469486
Eddie Naff <eddienaff@gmail.com>
2023-04-05 14:52:23 -0700

    Add example component

--- packages/r3f-basics/src/App.tsx
+++ packages/r3f-basics/src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { MyComponent } from "./components/my-component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
 }
 
 export default App;
