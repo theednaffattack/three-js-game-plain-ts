3aaf33e3411c960afa2cce38b2a4f2211a5c79d0
Eddie Naff <eddienaff@gmail.com>
2023-04-08 18:53:18 -0700

    Fix debug error

--- packages/mini-game/src/wheel-debug.tsx
+++ packages/mini-game/src/wheel-debug.tsx
@@ -1,17 +1,22 @@
 const debug = true;
 
 interface WheelDebugArgs {
+  debug?: boolean | undefined;
   radius: number;
   wheelRef: any;
 }
 
-export function WheelDebug({ radius, wheelRef }: WheelDebugArgs) {
-  return (
-    <group ref={wheelRef}>
-      <mesh rotation={[0, 0, Math.PI / 2]}>
-        <cylinderGeometry args={[radius, radius, 0.015, 16]} />
-        <meshNormalMaterial transparent={true} opacity={0.25} />
-      </mesh>
-    </group>
-  );
+export function WheelDebug({ debug, radius, wheelRef }: WheelDebugArgs) {
+  if (debug) {
+    return (
+      <group ref={wheelRef}>
+        <mesh rotation={[0, 0, Math.PI / 2]}>
+          <cylinderGeometry args={[radius, radius, 0.015, 16]} />
+          <meshNormalMaterial transparent={true} opacity={0.25} />
+        </mesh>
+      </group>
+    );
+  } else {
+    return null;
+  }
 }
